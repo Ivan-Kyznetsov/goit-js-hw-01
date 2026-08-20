43135da1790926af91411d4e6f0c19824c593ee9
Ivan <ivvankuznetsov@gmail.com>
2026-08-20 13:14:43 +0300

    task execution

---
+++ js/task-3.js
@@ -0,0 +1,10 @@
+'use strict';
+
+function getElementWidth(content, padding, border) {
+  const totalWidth = content + padding * 2 + border * 2;
+  return totalWidth;
+}
+
+console.log(getElementWidth(50, 8, 4)); // 74
+console.log(getElementWidth(60, 12, 8.5)); // 101
+console.log(getElementWidth(200, 0, 0)); // 200
